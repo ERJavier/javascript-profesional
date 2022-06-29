1fd5402ac749cc77637b9fe771158731856f31a1
Javier Cepeda <erjavierc@gmail.com>
2022-06-29 09:44:14 -0400

    intersection

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,12 +1,30 @@
 class AutoPause {
-    run(player) {
-        const observer = new IntersectionObserver(this.handlerIntersection, {threshold: 0.25})
-        observer.observe(player.media)
+  constructor() {
+    this.threshold = 0.25;
+    this.handleIntersection = this.handleIntersection.bind(this);
+  }
+
+  run(player) {
+    this.player = player;
+
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: this.threshold,
+    });
+
+    observer.observe(this.player.media);
+  }
+
+  handleIntersection(entries) {
+    const entry = entries[0];
+
+    const isVisible = entry.intersectionRatio >= this.threshold;
+
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
     }
-    handlerIntersection(entries){
-        const entry = entries[0]
-        console.log(entry)
-    }
+  }
 }
 
 export default AutoPause;
